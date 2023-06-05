2d23e1269e0b634fdc6534455d0de530b677e7b5
Shohei <shohei.shibata.dev@gmail.com>
2023-06-05 08:44:52 -0400

    add bot-field, remove action for debug

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -5,26 +5,35 @@ const ContactForm = () => {
   return (
     <form 
       id="contact-form" 
-      action='/success' 
-      className={contactFormStyles.form} 
       method='POST' 
+      className={contactFormStyles.form} 
       name='contact-form'
       data-netlify="true"
-      data-netlify-recaptcha="true"
+      data-netlify-honeypot="bot-field"
     >
       <input type='hidden' name='form-name' value='contact-form' />
+      <label htmlFor="bot-field" className={contactFormStyles.label} hidden>
+        Don't fill this out
+        <input name="botfield" type="text" required/>
+      </label>
+      <label htmlFor="name" className={contactFormStyles.label}>
+        Name
+        <input name="name" type="text" required/>
+      </label>
+      <label htmlFor="email" className={contactFormStyles.label}>
+        Email
+        <input name="email" type="email" required/>
+      </label>
+      <label htmlFor="message" className={contactFormStyles.label}>
+        Message
+        <textarea name="message" rows="10" required/>
+      </label>
       <input 
-        type="hidden" 
-        name="subject" 
-        value="contact-form" 
+        name="submit" 
+        type="submit" 
+        value="Submit" 
+        className={`btn btn-yellow ${contactFormStyles.submitButton}`}
       />
-      <label htmlFor="name" className={contactFormStyles.label}>Name</label>
-      <input name="name" type="text" required/>
-      <label htmlFor="email" className={contactFormStyles.label}>Email</label>
-      <input name="email" type="email" required/>
-      <label htmlFor="message" className={contactFormStyles.label}>Message</label>
-      <textarea name="message" rows="10" required/>
-      <input name="submit" type="submit" value="Submit" className={`btn btn-yellow ${contactFormStyles.submitButton}`}/>
     </form>
   )
 }
