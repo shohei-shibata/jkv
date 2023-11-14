4cda5eb0d4e3dadf1cee7487e993c9c26049ca22
Shohei <shohei.shibata.dev@gmail.com>
2023-11-14 12:15:12 -0500

    visual cleanup, mostly css

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,5 +1,5 @@
 import React from "react"
-import SectionTitle from "../components/section-title"
+import { Section } from "../components/section"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import * as projectStyle from "./project.module.css"
 import { Link } from "gatsby"
@@ -11,17 +11,17 @@ const ProjectPageTemplate = ({pageContext}) => {
   const { title, content, image, imageAltText } = pageContext
   return (
     <>
-      <div className={`center-content ${projectStyle.featuredImage}`}>
-        <GatsbyImage image={getImage(image)} alt={imageAltText}/>
-      </div>
-      <div className={`content-wrapper-default-width ${projectStyle.mainContentWrapper}`}>
-        <SectionTitle>{title}</SectionTitle>
-        <div dangerouslySetInnerHTML={{__html: content}} className="text-section-narrow-width"/>
+        <Section title="Projects">
+          <div className={`center-content ${projectStyle.featuredImage}`}>
+            <GatsbyImage image={getImage(image)} alt={imageAltText} />
+            <h1 className={projectStyle.postTitle}>{title}</h1>
+          </div>
+          <div dangerouslySetInnerHTML={{ __html: content }} className="text-section-narrow-width" />
+        </Section>
         <div className="btn-multiple-wrapper">
           <Link to="/" className="btn btn-yellow">Back to Home</Link>
           <Link to="/projects" className="btn btn-yellow">View Other Projects</Link>
         </div>
-      </div>
     </>
   )
 }
