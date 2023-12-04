3ac574c08689aa26f6b91fd24a80942f8304dc04
Shohei <shohei.shibata.dev@gmail.com>
2023-12-04 13:40:46 -0500

    remove html comments in seo tags

--- src/components/seo.js
+++ src/components/seo.js
@@ -12,20 +12,16 @@ const Seo = ({ title, description, image, pathname, children }) => {
   }
   return (
     <>
-
-      <!-- Primary Meta Tags -->
       <title>Meta Tags — Preview, Edit and Generate</title>
       <meta name="title" content={seo.title} />
       <meta name="description" content={seo.description} />
 
-      <!-- Open Graph / Facebook -->
       <meta property="og:type" content="website" />
       <meta property="og:url" content={seo.url} />
       <meta property="og:title" content={seo.title} />
       <meta property="og:description" content={seo.description} />
       <meta property="og:image" content={seo.image} />
 
-      <!-- Twitter -->
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content={seo.url} />
       <meta property="twitter:title" content={seo.title} />
@@ -33,7 +29,7 @@ const Seo = ({ title, description, image, pathname, children }) => {
       <meta property="twitter:image" content={seo.image} />
 
       {children}
-      
+
     </>
   )
 }
